07c4ca59313d7db5106b1818bfa41f454209b0b8
加缪 <jiamiu.hzy@alibaba-inc.com>
2014-10-26 00:37:28 +0800

    delete useless console.log, and auto release 0.0.6

--- index.js
+++ index.js
@@ -1,13 +1,57 @@
 var path = require('path'),
-  globalConfig = require(path.join(process.cwd(),'config')),
-  _ = require('lodash')
+  _ = require('lodash'),
+  fse = require("fs-extra"),
+  configPath = path.join(process.cwd(),'config.json')
 
-module.exports = {
+var mergeDefaults = _.partialRight(_.merge, function(a, b){
+  if( a===undefined){
+    return b
+  }else if(_.isArray(a) ){
+    return _.uniq(a.concat(b))
+  }else if(_.isObject(a)){
+    //return undefined meas go merge children
+    return undefined
+  }else{
+    return b
+  }
+})
+
+
+var requestModule = {
+  config : require(configPath),
   expand : function( module ){
+    if(!module.config) return
+
     var root = this
 
-    root[module.name] = module.config || {}
+    root.config[module.name] = root.config[module.name] || {}
     //this will override the default config
-    module.config = _.defaults( globalConfig[module.name] || {}, module.config || {})
+    mergeDefaults( root.config[module.name] , module.config )
+
+    module.config = root.config[module.name]
+  },
+  bootstrap : {
+    "function" : function(){
+      requestModule.dep.request.add("GET /config/list", function getConfig(req, res){
+        res.json( requestModule.config )
+      })
+
+      requestModule.dep.request.add("PUT /config/save", function updateConfig( req, res){
+        var inputConfig = req.param("config")
+        if(_.isObject( inputConfig)){
+          fse.outputJson( configPath,inputConfig,function(err){
+            if( err ){
+              ZERO.error(err)
+              return res.status(500).end()
+            }
+            mergeDefaults( requestModule.config, inputConfig )
+            res.json(inputConfig)
+          })
+        }
+      })
+    },
+    "order" : {before:"request.bootstrap"}
   }
 }
+
+module.exports = requestModule
